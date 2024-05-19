743e6c781d69f6455d86e4f138cb19d21fe0df7f
asura7969 <1402357969@qq.com>
2024-05-19 22:35:47 +0800

    add: JobInformation

---
+++ src/main/java/org/example/JobInformation.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import com.hazelcast.client.config.ClientConfig;
+import org.apache.seatunnel.common.utils.DateTimeUtils;
+import org.apache.seatunnel.common.utils.StringFormatUtils;
+import org.apache.seatunnel.engine.client.SeaTunnelClient;
+import org.apache.seatunnel.engine.client.job.JobMetricsRunner;
+import org.apache.seatunnel.engine.common.config.ConfigProvider;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * @author asura7969
+ * @create 2024-05-19-22:29
+ */
+public class JobInformation {
+
+    public static void main(String[] args) {
+        String clusterName = "";
+        ClientConfig clientConfig = ConfigProvider.locateAndGetClientConfig();
+
+        SeaTunnelClient engineClient = new SeaTunnelClient(clientConfig);
+        long jobId = 0L;
+        JobMetricsRunner.JobMetricsSummary jobMetricsSummary = (JobMetricsRunner.JobMetricsSummary) engineClient.getJobMetricsSummary(jobId);
+
+        //LocalDateTime startTime = LocalDateTime.now();
+        //LocalDateTime endTime = LocalDateTime.now();
+        String jobMetric = StringFormatUtils.formatTable(
+                "Job Statistic Information",
+                //"Start Time",
+                //DateTimeUtils.toString(
+                //        startTime, DateTimeUtils.Formatter.YYYY_MM_DD_HH_MM_SS),
+                //"End Time",
+                //DateTimeUtils.toString(
+                //        endTime, DateTimeUtils.Formatter.YYYY_MM_DD_HH_MM_SS),
+                //"Total Time(s)",
+                //Duration.between(startTime, endTime).getSeconds(),
+                "Total Read Count",
+                jobMetricsSummary.getSourceReadCount(),
+                "Total Write Count",
+                jobMetricsSummary.getSinkWriteCount(),
+                "Total Failed Count",
+                jobMetricsSummary.getSourceReadCount()
+                        - jobMetricsSummary.getSinkWriteCount());
+        System.out.println(jobMetric);
+
+    }
+}
